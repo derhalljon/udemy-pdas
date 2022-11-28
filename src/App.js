6acf4954d24b974fc6227caf0d830287d32a6492
HalldorjonD <77391518+derhalljon@users.noreply.github.com>
2022-11-28 16:41:45 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import ProfileCard from "./ProfileCard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <h1>Personal Digital Assistance</h1>
+
+                <ProfileCard />
+                <ProfileCard />
+                <ProfileCard />
+            </div>
+        </div>
+    );
 }
 
 export default App;
